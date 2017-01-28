fe1586da6a834064cd1f821b93a61bd42dd0d78e
Luke Powell <luke.powell@digipen.edu>
2017-01-27 17:47:49 -0800

    Refactored navigation, integrated fabric.io. Yaku list is default view

--- RiichiMahjongUtilitiesAndroid/app/src/main/java/com/dragonwellstudios/riichimahjongutilities/MainActivity.java
+++ RiichiMahjongUtilitiesAndroid/app/src/main/java/com/dragonwellstudios/riichimahjongutilities/MainActivity.java
@@ -1,9 +1,7 @@
 package com.dragonwellstudios.riichimahjongutilities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,9 +9,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
+import com.crashlytics.android.Crashlytics;
 import com.dragonwellstudios.riichimahjongutilities.yaku.YakuListFragment;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -21,27 +21,20 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        navigate(R.id.nav_yaku_list);
     }
 
     @Override
@@ -76,27 +69,22 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
+    private void navigate(int id) {
+        switch (id) {
+            case R.id.nav_yaku_list:
+                // Handle the camera action
+                YakuListFragment yakuListFragment = YakuListFragment.newInstance();
+                getSupportFragmentManager().beginTransaction().replace(R.id.content_main, yakuListFragment).commit();
+                break;
+        }
+    }
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_yaku_list) {
-            // Handle the camera action
-            YakuListFragment yakuListFragment = YakuListFragment.newInstance();
-            getSupportFragmentManager().beginTransaction().replace(R.id.content_main, yakuListFragment).commit();
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+        navigate(id);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
