9f3801d3dde94ec0da5b01e882e310115a06d48e
Luke Powell <luke.powell@digipen.edu>
2017-01-27 17:51:39 -0800

    Removed excess nav items, removed menu for the time being. Setup eventual menu for yaku list that will allow filtering of specific elements

--- RiichiMahjongUtilitiesAndroid/app/src/main/java/com/dragonwellstudios/riichimahjongutilities/MainActivity.java
+++ RiichiMahjongUtilitiesAndroid/app/src/main/java/com/dragonwellstudios/riichimahjongutilities/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        //getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -60,11 +60,6 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
